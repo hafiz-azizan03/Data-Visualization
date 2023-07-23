65c30f53ff73ccf44be1b106a1e2ec03b315b8cf
hafiz-azizan03 <139707724+hafiz-azizan03@users.noreply.github.com>
2023-07-23 18:23:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request, flash, redirect, url_for
 import pandas as pd
 import plotly.graph_objects as go
@@ -36,72 +35,79 @@ def upload_file():
     file.save(file_path)
 
     try:
+        # Determine the file extension
+        file_extension = os.path.splitext(file_path)[1]
+
         # Read the data from the file using pandas
-        df = pd.read_excel(file_path)  # Make sure pandas is installed
+        if file_extension == '.csv':
+            df = pd.read_csv(file_path)
+        else:
+            df = pd.read_excel(file_path, engine='openpyxl')
+
+        # Data manipulation and visualization
+        try:
+            # Ensure 'Category' column is accessed case-insensitively
+            category_column = df['Category'] if 'Category' in df.columns else df['category']
+
+            # Custom colors for the bar chart
+            bar_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2']
+
+            # Create individual charts using Plotly
+            fig1 = go.Figure(data=[go.Bar(x=category_column, y=df['Value'], marker_color=bar_colors)])
+            fig1.update_layout(
+                title='Bar Chart',
+                xaxis_title='Category',
+                yaxis_title='Value',
+                template='plotly'  # Use a light theme for aesthetics
+            )
+            chart_path1 = os.path.join('static', 'chart1.html')
+            fig1.write_html(chart_path1)
+
+            fig2 = go.Figure(data=[go.Line(x=category_column, y=df['Value'])])
+            fig2.update_layout(
+                title='Line Chart',
+                xaxis_title='Category',
+                yaxis_title='Value',
+                template='plotly'  # Use a light theme for aesthetics
+            )
+            chart_path2 = os.path.join('static', 'chart2.html')
+            fig2.write_html(chart_path2)
+
+            fig3 = go.Figure(data=[go.Pie(labels=category_column, values=df['Value'])])
+            fig3.update_layout(
+                title='Pie Chart',
+                template='plotly'  # Use a light theme for aesthetics
+            )
+            chart_path3 = os.path.join('static', 'chart3.html')
+            fig3.write_html(chart_path3)
+
+            fig4 = go.Figure(data=[go.Scatter(x=category_column, y=df['Value'], mode='markers')])
+            fig4.update_layout(
+                title='Scatter Plot',
+                xaxis_title='Category',
+                yaxis_title='Value',
+                template='plotly'  # Use a light theme for aesthetics
+            )
+            chart_path4 = os.path.join('static', 'chart4.html')
+            fig4.write_html(chart_path4)
+
+            # Remove the temporary file after a brief pause to release the file handle
+            time.sleep(1)  # Wait for a second to ensure the file handle is released
+            os.remove(file_path)
+
+            # Render the visualization template and pass the chart paths to display the charts
+            return render_template('visualization.html',
+                                   chart_path1=chart_path1,
+                                   chart_path2=chart_path2,
+                                   chart_path3=chart_path3,
+                                   chart_path4=chart_path4)
+
+        except Exception as e:
+            flash('Error processing the file: {}'.format(str(e)), 'error')
+            return redirect(url_for('index'))
+
     except Exception as e:
         flash('Error reading the file: {}'.format(str(e)), 'error')
-        return redirect(url_for('index'))
-
-    # Data manipulation and visualization
-    try:
-        # Ensure 'Category' column is accessed case-insensitively
-        category_column = df['Category'] if 'Category' in df.columns else df['category']
-
-        # Custom colors for the bar chart
-        bar_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2']
-
-        # Create individual charts using Plotly
-        fig1 = go.Figure(data=[go.Bar(x=category_column, y=df['Value'], marker_color=bar_colors)])
-        fig1.update_layout(
-            title='Bar Chart',
-            xaxis_title='Category',
-            yaxis_title='Value',
-            template='plotly_dark'  # Use a dark theme for aesthetics
-        )
-        chart_path1 = os.path.join('static', 'chart1.html')
-        fig1.write_html(chart_path1)
-
-        fig2 = go.Figure(data=[go.Line(x=category_column, y=df['Value'])])
-        fig2.update_layout(
-            title='Line Chart',
-            xaxis_title='Category',
-            yaxis_title='Value',
-            template='plotly_dark'  # Use a dark theme for aesthetics
-        )
-        chart_path2 = os.path.join('static', 'chart2.html')
-        fig2.write_html(chart_path2)
-
-        fig3 = go.Figure(data=[go.Pie(labels=category_column, values=df['Value'])])
-        fig3.update_layout(
-            title='Pie Chart',
-            template='plotly_dark'  # Use a dark theme for aesthetics
-        )
-        chart_path3 = os.path.join('static', 'chart3.html')
-        fig3.write_html(chart_path3)
-
-        fig4 = go.Figure(data=[go.Scatter(x=category_column, y=df['Value'], mode='markers')])
-        fig4.update_layout(
-            title='Scatter Plot',
-            xaxis_title='Category',
-            yaxis_title='Value',
-            template='plotly_dark'  # Use a dark theme for aesthetics
-        )
-        chart_path4 = os.path.join('static', 'chart4.html')
-        fig4.write_html(chart_path4)
-
-        # Remove the temporary file after a brief pause to release the file handle
-        time.sleep(1)  # Wait for a second to ensure the file handle is released
-        os.remove(file_path)
-
-        # Render the visualization template and pass the chart paths to display the charts
-        return render_template('visualization.html',
-                               chart_path1=chart_path1,
-                               chart_path2=chart_path2,
-                               chart_path3=chart_path3,
-                               chart_path4=chart_path4)
-
-    except Exception as e:
-        flash('Error processing the file: {}'.format(str(e)), 'error')
         return redirect(url_for('index'))
 
 
@@ -120,3 +126,4 @@ def dashboard():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
